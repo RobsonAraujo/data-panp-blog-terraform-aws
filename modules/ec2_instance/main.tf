3fafdb2184f14fd1d841e289b918b133911effb3
robshow <robsonaraujocarmo@hotmail.com>
2025-06-18 12:40:09 -0400

    feat: add role for Manager Session

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,9 +1,37 @@
 resource "aws_instance" "this" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami                  = var.ami
+  instance_type        = var.instance_type
+  key_name             = var.key_name
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
   tags = {
     Name = var.instance_name
   }
+}
+
+
+resource "aws_iam_role" "ssm_role" {
+  name = "ec2_ssm_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ec2_ssm_instance_profile"
+  role = aws_iam_role.ssm_role.name
 }
